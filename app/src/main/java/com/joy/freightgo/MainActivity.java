327855beeee25a6f4c6449c37282addde31d84f8
Joy <joy@hotmail.com>
2016-08-23 01:22:26 +0800

    Mark current location on to map by pressing the add icon

--- app/src/main/java/com/joy/freightgo/MainActivity.java
+++ app/src/main/java/com/joy/freightgo/MainActivity.java
@@ -18,6 +18,7 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -27,12 +28,23 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 /**
  * Created by Joy on 2016/8/20.
  */
 public class MainActivity extends Activity implements LocationListener,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        OnMapReadyCallback {
     private static final String joytag = "joydebug.mainactivity.";
 
     private static final int PERMISSION_LOCATION_REQUEST_CODE = 0;
@@ -40,6 +52,7 @@ public class MainActivity extends Activity implements LocationListener,
     private static final long UPDATE_LOCATION_INTERVAL_MS = 10000;
     private static final long UPDATE_LOCATION_FASTEST_INTERVAL_MS = 5000;
 
+    private MapFrag mMap;
     private TextView mPosition;
     private ImageView mSetting, mAddLocation;
 
@@ -47,6 +60,8 @@ public class MainActivity extends Activity implements LocationListener,
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
     private LocationRequest mLocationRequest;
+    private GoogleMap mGoogleMap;
+    private ArrayList<LatLng> mLatLngs;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,7 +75,17 @@ public class MainActivity extends Activity implements LocationListener,
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
         mAddLocation = (ImageView) findViewById(R.id.add_location);
+        mAddLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setMarkerOnMap();
+                Toast.makeText(getApplicationContext(), "Add location ("+mLastLocation.getLatitude()
+                        +", "+mLastLocation.getLongitude()+")", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
         mPosition = (TextView) findViewById(R.id.position);
         mSetting = (ImageView) findViewById(R.id.setting);
         mSetting.setOnClickListener(new View.OnClickListener() {
@@ -69,6 +94,8 @@ public class MainActivity extends Activity implements LocationListener,
                 startActivity(new Intent(getApplicationContext(), SettingActivity.class));
             }
         });
+
+        mLatLngs = new ArrayList<>();
 
         showPermissionDialog();
         mManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -138,21 +165,13 @@ public class MainActivity extends Activity implements LocationListener,
         if (mGoogleApiClient.isConnected()) startLocationUpdates();
     }
 
-    private void updatePositionText(Location location) {
-        Log.i(joytag+"updatePositionText()", ""+location);
-        StringBuilder builder = new StringBuilder();
-        builder.append(location.getLatitude()).append(" ")
-                .append(location.getLongitude());
-        mPosition.setText(builder.toString());
-    }
-
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
         if (mLastLocation != null) {
             Log.i(joytag+"onConnected()", "updatePositionText(mLastLocation);");
-            updatePositionText(mLastLocation);
+            updatePosition(mLastLocation);
         }
 
         createLocationRequest();
@@ -164,7 +183,9 @@ public class MainActivity extends Activity implements LocationListener,
                         builder.build());
 //        result.setResultCallback(this);
 
-        startLocationUpdates();
+        if (checkPermission(this)) {
+            startLocationUpdates();
+        }
     }
 
     @Override
@@ -198,7 +219,7 @@ public class MainActivity extends Activity implements LocationListener,
                         +", "+location.getAltitude()
                         +", "+location.getTime()
         );
-        updatePositionText(location);
+        updatePosition(location);
     }
 
     protected void startLocationUpdates() {
@@ -209,5 +230,76 @@ public class MainActivity extends Activity implements LocationListener,
     protected void stopLocationUpdates() {
         LocationServices.FusedLocationApi.removeLocationUpdates(
                 mGoogleApiClient, this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.i(joytag+"onMapReady(+)", "mLastLocation="+mLastLocation);
+        mGoogleMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // Add a marker in Taipei and move the camera
+        LatLng taipei = new LatLng(25.033408, 121.564099);
+        mGoogleMap.addMarker(new MarkerOptions().position(taipei).title("Marker in Taipei 101"));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(taipei));
+
+        // Create a CameraPosition object
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(taipei)
+                .zoom(17)
+                .build();
+        mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    private void updatePosition(Location location) {
+        if (location != null) {
+            mLastLocation = location;
+        }
+        updatePositionText(location);
+        moveMap(location);
+    }
+
+    private void updatePositionText(Location location) {
+        Log.i(joytag+"updatePositionText(+)", ""+location);
+        StringBuilder builder = new StringBuilder();
+        builder.append(location.getLatitude()).append(" ")
+                .append(location.getLongitude());
+        mPosition.setText(builder.toString());
+    }
+
+    private void moveMap(Location location) {
+        Log.i(joytag+"moveMap(+)", "mGoogleMap="+mGoogleMap+", location="+location);
+        if (mGoogleMap != null && location != null) {
+            LatLng newLocation = new LatLng(location.getLatitude(), location.getLongitude());
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(newLocation));
+            // Create a CameraPosition object
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(newLocation)
+                    .zoom(17)
+                    .build();
+            mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
+    }
+
+    private void setMarkerOnMap() {
+        Log.i(joytag+"setMarkerOnMap(+)", "+++");
+        if (mGoogleMap != null && mLastLocation != null) {
+            LatLng last = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            mLatLngs.add(last);
+            mGoogleMap.addMarker(new MarkerOptions().position(last).title("Location "+mLatLngs.size()));
+        }
     }
 }
--- app/src/main/java/com/joy/freightgo/MapFrag.java
+++ app/src/main/java/com/joy/freightgo/MapFrag.java
@@ -3,6 +3,7 @@ package com.joy.freightgo;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,39 +12,20 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFrag extends MapFragment implements OnMapReadyCallback {
+public class MapFrag extends MapFragment {
+    private static final String joytag = "joydebug.MapFrag.";
 
     private GoogleMap mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.i(joytag+"onCreateView(+)", "+++");
         View rootView = inflater.inflate(R.layout.fragment_maps, container, false);
-
-        getMapAsync(this);
-
         return rootView;
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
 }
